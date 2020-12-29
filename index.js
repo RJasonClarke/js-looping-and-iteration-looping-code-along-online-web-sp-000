23ce5bd32ba226f1904795b8eeef31c4b1530ffc
Robert Clarke <r.jason.clarke@gmail.com>
2020-12-29 20:11:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Code your solutions in this file
 
 const cards = ["Lisa", "Kaitlin", "Jan"];
-function writeCards(cards) {
+function writeCards(cards, event) {
+  let birthday =[];
   for (let i = 0; i < writeCards.length; i++) {
     console.log("Thank you ${cards[i]} for the gift")
   }
