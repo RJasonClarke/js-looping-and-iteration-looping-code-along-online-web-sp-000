bfb9d613b791c1f8535b5df075068dafac04ff07
Robert Clarke <r.jason.clarke@gmail.com>
2020-12-29 19:55:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+
+const cards = ["Ada", "Brendan", "Ali"];
+function writeCards(cards) {
+  for (let i = 0; i < writeCards.length; i++) {
+    console.log("Thank you ${cards[i]} for the gift")
+  }
+
+  return gifts;
+}
